0fe2589a544d204134ddfc44435d791e3e9cab77
mike <michalbaz@poczta.onet.pl>
2020-12-13 01:01:24 +0100

    controls fixed, comments added, part of docs added

--- main.c
+++ main.c
@@ -8,22 +8,23 @@
 
 #define T1_BUZZ 50
 #define T2_BUZZ 25
-#define T1_CONTROLS 5
-#define T2_CONTROLS 10
+#define T1_CONTROLS 30
+#define T2_CONTROLS 100
 
 #define ALARM_MASK 0x01
 
 const uint8_t EEMEM PS_controls[] = {
-    0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x00, 0x02,
-    0x00, 0x00, 0x00, 0x04, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    0x00, 0x00, 0x02,0x00, 0x00, 0x00, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+    0x00, 0x00, 0x04, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
 const uint8_t EEMEM PW_controls[] = {
-    2, 3, 1, 3, 4, 5, 3, 1, 3, 4, 5, 0, 1, 6, 4, 5, 6, 1, 6, 4, 5, 0, 7, 0
+    2, 3, 1, 3, 4, 5, 1, 3, 1, 3, 4, 5, 0, 1, 7, 4,
+    5, 1, 7, 1, 7, 4, 5, 0, 6, 0
 };
 
 const uint8_t EEMEM PA_controls[] = {
-    12, 0, 0, 0, 22, 3, 0, 0, 0, 22, 8, 5, 0, 0, 22, 13, 0, 0, 0, 22, 18, 15, 22, 0
+    13, 0, 0, 0, 24, 3, 0, 0, 0, 0, 24, 9, 6, 0, 0, 24, 14, 0, 0, 0, 0, 24, 20, 17, 24, 0
 };
 
 const uint8_t EEMEM PS_buzz[] = {
@@ -56,7 +57,7 @@ int main(void){
     char x = 0;
     uint8_t S1, S2, pc_controls = 0; 
     char control_out, control_cond;
-    uint16_t tim_controls = 0;
+    uint8_t tim_controls = 0;
     /*--------------control vars end---------------*/
 
     uint8_t flags = 0x00;
@@ -70,12 +71,11 @@ int main(void){
 
     while(1){
         update_controls(); //update controls status
-        S1 = incK;
-        S2 = decK;
-        if(x >= 16) x = 0;
+        S1 = incK; //store new S1 control status
+        S2 = decK; //same as above
+        if(x >= 16) x = 0; 
         else if(x < 0) x = 15;
-        PORTD = ~pc_controls;
-        _delay_ms(500);
+        PORTD = ~x; //for testing causes
         /*-------------------controls graph start--------------------------*/
         control_out = eeprom_read_byte(&PS_controls[pc_controls]); //get output setup            
         switch(eeprom_read_byte(&PW_controls[pc_controls])){ //check condition
@@ -85,18 +85,18 @@ int main(void){
             case 3: control_cond = S1; break;
             case 4: control_cond = !S1 || !S2; break;
             case 5: control_cond = !tim_controls; break;
-            case 6: control_cond = S2; break;
-            case 7: control_cond = !S1 && !S2; break;
+            case 7: control_cond = S2; break;
+            case 6: control_cond = !S1 && !S2; break;
         }
-        if(control_cond){
+        if(control_cond){ //if condition is met, increase pc and go to the next instruction
             ++pc_controls;
         }
         else{
-            pc_controls = eeprom_read_byte(&PA_controls[pc_controls]);
+            pc_controls = eeprom_read_byte(&PA_controls[pc_controls]); //else jump
         }
-        if(control_out & 0x08) { ++x; }
+        if(control_out & 0x08) { ++x; } //set "output"
         if(control_out & 0x04) { --x; }
-        if(control_out & 0x02) { tim_controls = T1_CONTROLS; }
+        if(control_out & 0x02) { tim_controls = T1_CONTROLS; } //set timers
         if(control_out & 0x01) { tim_controls = T2_CONTROLS; }
         /*---------------------controls graph end-------------------------*/
 
@@ -121,20 +121,20 @@ int main(void){
 
         if(buz_out & 0x80)PORTC = 0x01;  //turn buzzer on
         else PORTC = 0x00; //turn buzzer off
-        if(buz_out & 0x40) { tim_buzz = T1_BUZZ; }
+        if(buz_out & 0x40) { tim_buzz = T1_BUZZ; } //set timers
         if(buz_out & 0x20) { tim_buzz = T2_BUZZ; }
         if(buz_out & 0x10) { tim_buzz = 4 * T1_BUZZ; }
         if(buz_out & 0x08) { tim_buzz = 6 * T2_BUZZ; }
-        if(buz_out & 0x04) { i_buzz = 10; }
-        if(buz_out & 0x02) { i_buzz = 5; }
+        if(buz_out & 0x04) { i_buzz = 10; } //set iterators
+        if(buz_out & 0x02) { i_buzz = 5; } //decrease iterator
         if(buz_out & 0x01) { --i_buzz; }
         /*---------------------buzzer graph end-------------------------*/
-        while(!cycle){
-            continue;
+        while(!cycle){ //while cycle has not lasted 1/400s
+            continue; //stay in the loop
         }
         cycle = 0;
-        if(tim_buzz) --tim_buzz;
-        if(tim_controls) --tim_controls;
+        if(tim_buzz) --tim_buzz; //decrease buzzer timer if > 0 
+        if(tim_controls) --tim_controls; //decrease controls timer if > 0
     }
 }
 
